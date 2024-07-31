6247e6b1c196caa547452a0a263b008329554595
Florian Hübner <fhuebner@posteo.de>
2024-07-31 18:35:14 +0200

    actually include shaded colors

--- src/main/java/lol/hub/mapcolors/Main.java
+++ src/main/java/lol/hub/mapcolors/Main.java
@@ -6,38 +6,40 @@ import net.minecraft.world.level.material.MapColor;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
 public class Main implements ModInitializer {
+
     @Override
     public void onInitialize() {
 
-        var table = Arrays.stream(MapColor.MATERIAL_COLORS)
+        var bases = Arrays.stream(MapColor.MATERIAL_COLORS)
                 .filter(Objects::nonNull)
                 .sorted(Comparator.comparingInt(value -> value.id))
                 .toList();
 
-        StringBuilder csv = new StringBuilder();
-        for (MapColor color : table) {
-            var a = color.id > 0 ? 255 : 0;
-            var rgba = (a << 24) | color.col;
-            var r = (rgba >> 16) & 0xFF;
-            var g = (rgba >> 8) & 0xFF;
-            var b = rgba & 0xFF;
-            csv
-                    .append(color.id).append(",")
-                    .append(r).append(",")
-                    .append(g).append(",")
-                    .append(b).append(",")
-                    .append(a).append(",")
-                    .append(String.format("%02X%02X%02X", r, g, b))
-                    .append("\n");
+        var table = new ArrayList<String>();
+
+        for (MapColor base : bases) {
+            for (MapColor.Brightness shade : MapColor.Brightness.VALUES) {
+                var a = base.id > 3 ? 255 : 0;
+                var rgba = base.calculateRGBColor(shade);
+                var r = rgba & 0xFF;
+                var g = (rgba >> 8) & 0xFF;
+                var b = (rgba >> 16) & 0xFF;
+                String line = base.id * 4 + shade.id
+                        + "," + r + "," + g + "," + b + "," + a + "," +
+                        String.format("%02X%02X%02X", r, g, b);
+                table.add(line);
+            }
         }
 
         try {
-            Files.write(Path.of("colors.csv"), csv.toString().getBytes());
+            Files.write(Path.of("colors.csv"),
+                    String.join("\n", table).getBytes());
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
